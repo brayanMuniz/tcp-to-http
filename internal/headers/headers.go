d13de90e74a87310b07f68a1446657c8f208aa83
Brayan Muniz <35124364+brayanMuniz@users.noreply.github.com>
2025-07-20 08:19:37 -0500

    check for correct keys

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,9 +3,11 @@ package headers
 import (
 	"fmt"
 	"strings"
+	"unicode"
 )
 
-const crlf = "\r\n" // This seperates headers
+// NOTE: \r and \n is each one byte
+const crlf = "\r\n"
 
 type Headers map[string]string
 
@@ -17,11 +19,12 @@ func (h Headers) Parse(data []byte) (bytesCounsumed int, finishedParsing bool, e
 	dataString := string(data)
 
 	crlfIndex := strings.Index(dataString, crlf)
+	// more data needed
 	if crlfIndex == -1 {
 		return 0, false, nil
 	}
 
-	// done finding heders
+	// done
 	if crlfIndex == 0 {
 		return 0, true, nil
 	}
@@ -33,11 +36,27 @@ func (h Headers) Parse(data []byte) (bytesCounsumed int, finishedParsing bool, e
 		return 0, false, fmt.Errorf("header not formatted correctly")
 	}
 
-	headerKey := headerParts[0][:len(headerParts[0])-1]
+	headerKey := headerParts[0][:len(headerParts[0])-1] // remove the :
 	headerValue := headerParts[1]
 
+	// Check if key is valid
+	for _, c := range headerKey {
+		if unicode.IsLetter(c) || unicode.IsDigit(c) {
+			continue
+		}
+
+		var specialCharacters = []rune{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~'}
+		for _, s := range specialCharacters {
+			if c == s {
+				continue
+			}
+		}
+
+		return 0, false, fmt.Errorf("header key is not valid")
+	}
+
+	headerKey = strings.ToLower(headerKey)
 	h[headerKey] = headerValue
 
-	return crlfIndex + 2, false, nil
-
+	return crlfIndex + 2, false, nil // + 2 for the crlf
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -6,21 +6,51 @@ import (
 	"testing"
 )
 
-// go test ./...
-
 func TestHeadersParse(t *testing.T) {
 
-	// Test: Valid single header
+	// Valid single header
 	headers := NewHeaders()
 	data := []byte("Host: localhost:42069\r\n\r\n")
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 
-	// Test: Invalid spacing header
+	// Valid single header, extra white space
+	headers = NewHeaders()
+	data = []byte("  Host: localhost:42069  \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 27, n)
+	assert.False(t, done)
+
+	// Valid 2 headers, extra white space
+	headers = NewHeaders()
+	data = []byte("  Host: localhost:42069\r\n Anime: forSure  \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, 25, n)
+	assert.Equal(t, "localhost:42069", headers["host"])
+
+	n, done, err = headers.Parse(data[n:]) // continue to where it left off
+	assert.Equal(t, 19, n)
+	assert.Equal(t, "forSure", headers["anime"])
+	assert.False(t, done)
+
+	// Invalid key
+	headers = NewHeaders()
+	data = []byte("H@st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Invalid spacing header
 	headers = NewHeaders()
 	data = []byte("       Host : localhost:42069       \r\n\r\n")
 	n, done, err = headers.Parse(data)
